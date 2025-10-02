5f792a3b74dbc2d1663872635e83b193e100cf19
Thiago Henrique Machado Filho <thiago.filho@dinfor.uniceub>
2025-10-02 10:26:39 -0300

    Ajuste thread hotfix

--- master-master.py
+++ master-master.py
@@ -14,7 +14,7 @@ class MasterCoordinator:
         self.socket_server.bind((self.host, self.port))
         self.socket_server.listen(5)
 
-        print(f"[Coordinator] Master {self.server_id} pronto em {self.host}:{self.port}")
+        print(f"[Coordinator] Master ID4 pronto em {self.host}:{self.port}")
         threading.Thread(target=self.listen_for_masters, daemon=True).start()
 
     def add_neighbor(self, neighbor_host, neighbor_port):
@@ -57,27 +57,29 @@ class MasterCoordinator:
 
     def send_heartbeat(self):
         """Envia heartbeat para todos os vizinhos"""
-        for neighbor_host, neighbor_port in self.neighbors:
-            try:
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.connect((neighbor_host, neighbor_port))
+        while True:
+            for neighbor_host, neighbor_port in self.neighbors:
+                try:
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    s.connect((neighbor_host, neighbor_port))
 
-                msg = {
-                    "SERVER_ID": "ID4 DO KAWEZAO",
-                    "TASK": "HEARTBEAT"
-                }
-                s.sendall(json.dumps(msg).encode("utf-8"))
-                print(f"[Coordinator] Enviou HEARTBEAT → {neighbor_host}:{neighbor_port}")
+                    msg = {
+                        "SERVER_ID": "ID4 DO KAWEZAO",
+                        "TASK": "HEARTBEAT"
+                    }
+                    s.sendall(json.dumps(msg).encode("utf-8"))
+                    print(f"[Coordinator] Enviou HEARTBEAT → {neighbor_host}:{neighbor_port}")
 
-                # Espera resposta
-                response = s.recv(1024)
-                if response:
-                    print(f"[Coordinator] Resposta de {neighbor_host}:{neighbor_port} → {response.decode()}")
+                    # Espera resposta
+                    response = s.recv(1024)
+                    if response:
+                        print(f"[Coordinator] Resposta de {neighbor_host}:{neighbor_port} → {response.decode()}")
 
-                s.close()
+                    s.close()
+                    time.sleep(5)
 
-            except Exception as e:
-                print(f"[Coordinator] Falha ao enviar HEARTBEAT para {neighbor_host}:{neighbor_port} → {e}")
+                except Exception as e:
+                    print(f"[Coordinator] Falha ao enviar HEARTBEAT para {neighbor_host}:{neighbor_port} → {e}")
 
 
 if __name__ == "__main__":
@@ -90,11 +92,12 @@ if __name__ == "__main__":
     # exemplo de vizinhos (ajuste as portas conforme seu setup)
     master.add_neighbor("10.62.217.209", 5000)
     master.add_neighbor("10.62.217.203", 5000)
+    master.add_neighbor("10.62.217.16", 5000)
     master.add_neighbor("10.62.217.199", 8765)
 
     try:
-        while True:
-            master.send_heartbeat()  # envia heartbeat a cada 5s
-            time.sleep(5)
+        hbeat_send = threading.Thread(target=master.send_heartbeat, daemon=True, args=())
+        hbeat_send.start()
+        hbeat_send.join()
     except KeyboardInterrupt:
         print("[Coordinator] Finalizado.")
